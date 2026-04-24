0d50ceba0e38bf68cb34efafaf77974daa54b8c4
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-04-24 12:00:06 +0000

    Initial commit with Person Non Individual and household skeleton

---
+++ knaps/knaps/doctype/knaps_phone_number/knaps_phone_number.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, KNAPS and Contributors and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class KNAPSPhoneNumber(Document):
+	pass

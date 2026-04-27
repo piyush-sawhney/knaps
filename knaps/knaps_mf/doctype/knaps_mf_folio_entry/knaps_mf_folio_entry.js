b8a2506cfaf2bb0dc9d53bb37d5d2eb64fd09086
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-04-27 12:07:57 +0000

    creating mf and li

---
+++ knaps/knaps_mf/doctype/knaps_mf_folio_entry/knaps_mf_folio_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, KNAPS and Contributors and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("KNAPS MF Folio Entry", {
+// 	refresh(frm) {
+
+// 	},
+// });

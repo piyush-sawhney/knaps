f18c7e8770c68a75760890d7c1870a8a60489d63
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-05-12 12:42:48 +0000

    improving lead

--- knaps/knaps_lead_management/doctype/knaps_lead/knaps_lead.js
+++ knaps/knaps_lead_management/doctype/knaps_lead/knaps_lead.js
@@ -2,32 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("KNAPS Lead", {
-	salutation: function(frm) {
-		frm.trigger("update_lead_name");
-	},
-	first_name: function(frm) {
-		frm.trigger("update_lead_name");
-	},
-	middle_name: function(frm) {
-		frm.trigger("update_lead_name");
-	},
-	last_name: function(frm) {
-		frm.trigger("update_lead_name");
-	},
-	update_lead_name: function(frm) {
-		const parts = [];
-		if (frm.doc.salutation) parts.push(frm.doc.salutation);
-		if (frm.doc.first_name) parts.push(frm.doc.first_name);
-		if (frm.doc.middle_name) parts.push(frm.doc.middle_name);
-		if (frm.doc.last_name) parts.push(frm.doc.last_name);
-		frm.set_value("lead_name", parts.join(" ").trim());
-	},
 	refresh: function(frm) {
 		if(!frm.doc.__islocal) {
   		frappe.contacts.render_address_and_contact(frm);
     } else {
   		frappe.contacts.clear_address_and_contact(frm);
-	}
-		frm.trigger("update_lead_name");
-	}
+	}}
 });
--- knaps/knaps_lead_management/doctype/knaps_lead/knaps_lead.py
+++ knaps/knaps_lead_management/doctype/knaps_lead/knaps_lead.py
@@ -14,16 +14,3 @@ class KNAPSLead(Document):
 
 	def on_trash(self):
 		delete_contact_and_address("Member", self.name)
-		
-	def validate(self):
-		self.update_lead_name()
-
-	def update_lead_name(self):
-		"""Construct lead name from salutation, first, middle, and last name"""
-		salutation = (self.salutation or "").strip()
-		first = (self.first_name or "").strip()
-		middle = (self.middle_name or "").strip()
-		last = (self.last_name or "").strip()
-
-		parts = [salutation, first, middle, last]
-		self.lead_name = " ".join([part for part in parts if part])

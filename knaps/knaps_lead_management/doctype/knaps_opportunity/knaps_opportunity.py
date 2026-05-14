7174446b19c92d85f72ed7dc891c2478684f40c6
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-05-14 09:43:26 +0000

    adding opportunity and lead dashboard

--- knaps/knaps_lead_management/doctype/knaps_opportunity/knaps_opportunity.py
+++ knaps/knaps_lead_management/doctype/knaps_opportunity/knaps_opportunity.py
@@ -6,4 +6,29 @@ from frappe.model.document import Document
 
 
 class KNAPSOpportunity(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+		from knaps.knaps_lead_management.doctype.knaps_lead_interest.knaps_lead_interest import KNAPSLeadInterest
+
+		client: DF.Link
+		client_name: DF.Data | None
+		email: DF.Data | None
+		internal_notes: DF.TextEditor | None
+		opportunity_type: DF.TableMultiSelect[KNAPSLeadInterest]
+		phone: DF.Phone | None
+		source: DF.Link
+		status: DF.Literal["New", "Qualified", "Nurture", "Prospect", "Won", "Lost", "Junk"]
+		utm_campaign: DF.Data | None
+		utm_content: DF.Data | None
+		utm_medium: DF.Data | None
+		utm_source: DF.Data | None
+		utm_term: DF.Data | None
+		whatsapp: DF.Phone | None
+	# end: auto-generated types
+
 	pass

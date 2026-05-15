6238c02582d99dd40d14a152b5cfe633b4dcc1a0
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-05-15 18:14:56 +0000

    Individual Profile

---
+++ knaps/knaps/doctype/knaps_individual_profile/knaps_individual_profile.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2026, KNAPS and Contributors and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class KNAPSIndividualProfile(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		country_of_birth: DF.Link | None
+		educational_qualification: DF.Literal[
+			"",
+			"7th Pass",
+			"10th Pass",
+			"12th Pass",
+			"Graduate",
+			"Post Graduate",
+			"Doctorate Holder",
+			"Professional",
+		]
+		income_slab: DF.Literal[
+			"", "Below 1 Lakh", "1 - 5 Lakh", "5 - 10 Lakh", "10- 25 Lakh", "25 Lakh - 1 Crore", "Above 1 Cr"
+		]
+		marital_status: DF.Literal["", "Single", "Married", "Divorced", "Widowed", "Separated"]
+		nationality: DF.Link | None
+		number_of_dependents: DF.Int
+		occupation: DF.Literal[
+			"",
+			"Business",
+			"Service",
+			"Professional",
+			"Agriculture",
+			"Retired",
+			"Housewife",
+			"Student",
+			"Others",
+		]
+		occupational_details: DF.Data | None
+		person: DF.Link
+		politically_exposed_person: DF.Literal["", "Yes", "No", "Related to PEP"]
+		title: DF.Data | None
+	# end: auto-generated types
+
+	def before_save(self):
+		if self.person:
+			person_title = frappe.db.get_value("KNAPS Person", self.person, "full_name")
+			self.title = f"{person_title} Profile"

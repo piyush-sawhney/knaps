8c6df42a9f3c1fa7a8d04d0e0e490adee87bbb5a
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-05-27 11:16:57 +0000

    knaps payment

--- knaps/knaps/doctype/knaps_payment/knaps_payment.py
+++ knaps/knaps/doctype/knaps_payment/knaps_payment.py
@@ -14,6 +14,7 @@ class KNAPSPayment(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		bank_account_number: DF.Link | None
 		parent: DF.Data
 		parentfield: DF.Data
 		parenttype: DF.Data

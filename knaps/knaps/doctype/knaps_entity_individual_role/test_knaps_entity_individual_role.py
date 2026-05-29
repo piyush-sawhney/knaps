0c99f998f91553ad148bdc64b3c22b16aced5acd
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-05-29 10:26:07 +0000

    knaps entity role

--- knaps/knaps/doctype/knaps_entity_individual_role/test_knaps_entity_individual_role.py
+++ knaps/knaps/doctype/knaps_entity_individual_role/test_knaps_entity_individual_role.py
@@ -11,9 +11,9 @@ EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
 
-class IntegrationTestKNAPSEntityRole(IntegrationTestCase):
+class IntegrationTestKNAPSEntityIndividualRole(IntegrationTestCase):
 	"""
-	Integration tests for KNAPSEntityRole.
+	Integration tests for KNAPSEntityIndividualRole.
 	Use this class for testing interactions between multiple components.
 	"""
 
